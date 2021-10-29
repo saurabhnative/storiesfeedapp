c87b59aab56806530d45dadf7eeb5ca2be6263a3
Saurabh Mhatre <codeclassifiers@gmail.com>
2021-10-29 23:00:23 +0530

    Test cases added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Home from "./components/Home";
+import categoryItems from "./dataSource/categories";
+test("renders header in main screen", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/stories feed/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test("renders categories in main screen", () => {
+  render(<Home />);
+  categoryItems.map((item) => {
+    const categories = screen.getAllByText(
+      new RegExp(item.name.toLowerCase(), "i")
+    );
+    categories.map((element) => {
+      expect(element).toBeInTheDocument();
+    });
+  });
 });
